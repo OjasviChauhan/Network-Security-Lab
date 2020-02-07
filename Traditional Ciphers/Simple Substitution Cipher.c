8d3eb7709f9c90d82fbb83ec5d0f8ed0ab120266
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-02-07 20:18:26 +0530

    Update Simple Substitution Cipher.c

--- Traditional Ciphers/Simple Substitution Cipher.c
+++ Traditional Ciphers/Simple Substitution Cipher.c
@@ -8,11 +8,9 @@ int main()
     char Key[26]={'p','h','q','g','i','u','m','e','a','y','l','n','o','f','d','x','j','k','r','c','v','s','t','z','w','b'};
     int i,j;
     for(i=0;i<strlen(A);i++){
-        for(j=0;j<26;j++){
-            if(A[i]==Key[j]){
+        for(j=0;j<26;j++)
+            if(A[i]==Key[j])
                 x=j;
-            }
-        }
         A[i]='a'+x;
     }
     printf("Plain text : ");
