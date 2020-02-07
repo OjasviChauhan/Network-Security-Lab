c1eccce8fb784d318c7069936aa6360d7eee2724
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-02-07 20:17:30 +0530

    Update Caesar Cipher.c

--- Traditional Ciphers/Caesar Cipher.c
+++ Traditional Ciphers/Caesar Cipher.c
@@ -1,32 +1,26 @@
 #include<stdio.h>
-
 int main()
 {
 	char message[100], ch;
 	int i, key;
-
+	
 	printf("Enter a message to Decrypt: ");
 	gets(message);
 	printf("Enter key: ");
 	scanf("%d", &key);
-
+	
 	for(i = 0; message[i] != '\0'; ++i){
 		ch = message[i];
-
 		if(ch >= 'a' && ch <= 'z'){
 			ch = ch + key;
-
-			if(ch > 'z'){
-				ch = ch - 'z' + 'a' - 1;
-			}
+			if(ch > 'z')
+			    ch = ch-'z'+'a'-1;
 			message[i] = ch;
 		}
 		else if(ch >= 'A' && ch <= 'Z'){
 			ch = ch + key;
-
-			if(ch > 'Z'){
-				ch = ch - 'Z' + 'A' - 1;
-			}
+			if(ch > 'Z')
+			    ch = ch-'Z'+'A'-1;
 			message[i] = ch;
 		}
 	}
